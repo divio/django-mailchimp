3765f4a1f812d6c4495f1fd51d8c0f3543401496
ojii <jonas.obrist@divio.ch>
2010-05-30 18:18:37 +0200

    fixed cron

--- mailchimp/cron.py
+++ mailchimp/cron.py
@@ -1,8 +1,10 @@
 from cronjobs.base import Cron
+from cronjobs.constants import MINUTES
 from mailchimp.utils import dequeue
 
 class DequeueCron(Cron):
-    run_every = 1 # run every 30 seconds
+    run_every = 1
+    interval_unit = MINUTES
  
     def job(self):
         try:
