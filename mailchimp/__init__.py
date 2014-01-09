553f0072ea65faa2c68e04afe7056ed2c7c15871
Benjamin Wohlwend <piquadrat@gmail.com>
2014-01-09 15:48:31 +0100

    version bump

--- mailchimp/__init__.py
+++ mailchimp/__init__.py
@@ -4,4 +4,4 @@ Note about version numbers:
 The version number is tied to the API version of mailchimp that we support.
 Version 1.3.0 indicated the first release supporting v1.3 of the API.
 """
-__version__ = '1.3.0'
+__version__ = '1.3.1'
