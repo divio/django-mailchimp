c80ec75f599df6417740417bbb0614c2b6e1fa93
Paulo <commonzenpython@gmail.com>
2017-01-05 06:07:05 -0500

    Bumped version to 2.0.0a2

--- mailchimp/__init__.py
+++ mailchimp/__init__.py
@@ -4,4 +4,4 @@ Note about version numbers:
 The version number is tied to the API version of mailchimp that we support.
 Version 1.3.0 indicated the first release supporting v1.3 of the API.
 """
-__version__ = '2.0.0a1'
+__version__ = '2.0.0a2'
